bc2a738210297f0697401f7bf8d065c4267800fa
sanghp1801 <sang@chott.io>
2025-02-26 22:45:29 +0700

    add test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -7,5 +7,19 @@ export default {
     '^@/(.*)$': '<rootDir>/src/$1', // Cấu hình absolute import @/
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy' // Mock CSS modules
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.tsx',
+    '!src/reportWebVitals.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
 }
